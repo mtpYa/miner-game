97b11df01dce04e22dbe8c1931c73d656e027a7e
mtpya <19mtp83@gmail.com>
2017-08-01 01:10:35 +0300

    some changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { name } from './firstModule';
+import './style/canvas.css';
 
-console.log(`This is the ${name}`);
+document.addEventListener('DOMContentLoaded', () => {
+  const coordContainer = document.getElementById('coordInfo');
+  const canvas = document.getElementById('canv');
+  const context = canvas.getContext('2d');
 
+  context.strokeStyle = '#006400';
+  context.fillStyle = '#94a83e';
+  context.lineWidth = 3;
+
+  context.beginPath();
+  context.arc(281, 281, 100, Math.PI / 180 * 45, Math.PI / 180 * 315, false);
+  context.lineTo(281, 281);
+  context.closePath();
+
+  context.fill();
+  context.stroke();
+
+  canvas.addEventListener('mousemove', (e) => {
+    // const coordBox = coordContainer.getBoundingClientRect();
+    coordContainer.innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`;
+  });
+});
